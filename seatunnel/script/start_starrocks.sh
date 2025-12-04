8ab21a19dda70a732d11745f376873f2d84e6aa4
Colin Chen <hansichan.crypto@gmail.com>
2025-12-04 10:34:23 +0800

    update

--- seatunnel/script/start_starrocks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# 启动 StarRocks all-in-one 容器
-cd "$(dirname "$0")/.." || exit 1
-
-docker compose up -d starrocks
--- seatunnel/script/stop_starrocks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# 停止 StarRocks all-in-one 容器
-cd "$(dirname "$0")/.." || exit 1
-
-docker compose stop starrocks
